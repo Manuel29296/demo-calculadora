cb889e4c5711abf24efb8d47e99084413f71fef8
Manuel Carmona <juan.carmona29296@ucaldas.edu.co>
2024-09-09 08:10:51 -0500

    Implementación de resta, multiplicación y division

---
+++ main.py
@@ -0,0 +1,23 @@
+def suma(x , y):
+    return x+y
+
+resultado=suma(5,3)
+print(resultado)
+
+def resta(x,y):
+    return x-y
+
+resultado = resta(4-2)
+print(resultado)
+
+def multiplicacion(x,y):
+    return x*y
+
+resultado = multiplicacion(3,2)
+print(resultado)
+
+def division(x,y):
+    return x/y
+
+resultado = division(6,2)
+print(resultado)
